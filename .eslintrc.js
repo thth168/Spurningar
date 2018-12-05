fa3abd266569ee858d180183793acd79adb09f1c
thth168 <43797464+thth168@users.noreply.github.com>
2018-12-05 21:53:59 +0000

    ble

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     rules: {
       'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
       'class-methods-use-this': 0,
+      "quotes": [2, "single", { "avoidEscape": true }]
     },
     plugins: ['import'],
   };
